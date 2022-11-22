13a16b7a76c1f58b0eb50457124087e027740643
SomeBottle <somebottle@outlook.com>
2022-11-22 19:20:48 +0800

    Finally I receive an AC

--- Algo/code/C-Cpp/Lanqiao/BASIC/BASIC-19.cpp
+++ Algo/code/C-Cpp/Lanqiao/BASIC/BASIC-19.cpp
@@ -14,66 +14,51 @@ int main()
         scanf(" %c", &temp);
         myStr[i] = temp;
     }
-    // 字符串长度是否是奇数
-    bool isOdd = strLen & 1;
-    // 是否已经有【不成对】的字符了，对于【奇数长】的回文串中，有且仅能有一个不成对的字符，比如acbca中的b
-    bool hasAloneChar = false;
-    // 字符串中间一位的下标
-    int middleInd = (strLen - 1) / 2;
     // 纪录交换次数
     int swapCount = 0;
-    // 开始处理字符串
-    // former最先选中一个字符
-    /*
-        former从第0个字符扫描到字符串中间的一位
-        为什么只用到中间? 我经过多次实践才得出这个推论：
-            💡在交换相邻字符的时候，
-                - 如果右半侧【有两个相同的字符】，总会有【其中一个】被“挤”到左侧
-                - 而如果右半侧有【一个落单的字符】，它则总会被“挤”到正中心
-    */
-    for (int former = 0; former <= middleInd; former++)
+    // 字符串中间的下标
+    int middleInt = (strLen - 1) / 2;
+    // 字符串长度是否是奇数
+    bool isOdd = strLen & 1;
+    // 是否已经存在【落单】的字符，在回文串【长度为奇数】时，【有且只能】有一个落单字符
+    bool hasAlone = false;
+    // symmetry指向【结果中】与【former指向的字符】对称的字符下标
+    int symmetry = strLen - 1;
+    // former指向一个字符，从0扫描到字符串尾部
+    for (int former = 0; former < strLen; former++)
     {
-        int destInd = strLen - 1 - former; // 算出和former对称的下标，也就是要将latter的字符一直交换到下标为destInd
-        // 如果和former对称的下标destInd指向的字符也是一致的，已经配对了，就不用接着往下了
-        if (myStr[former] == myStr[destInd])
-            continue;
-
-        int latter = -1;
-        // 找到一个【和former对应的字符相同的字符】，i从former的下一位开始扫描到字符串尾部
-        for (int i = former + 1; i < strLen; i++)
+        // latter指向另一个字符，从symmetry扫描到former指向的字符
+        for (int latter = symmetry; latter >= former; latter--)
         {
-            if (myStr[former] == myStr[i])
-            {
-                int symmetry = strLen - 1 - i; // 算出与i下标对称的下标
-                /*
-                    比如aabba, 如果former指向的是第二个a, i此时会等于最后一个a的下标，
-                    但是这个a已经和第一个a对称了，这个时候就需要跳过了。
-                */
-                if (myStr[i] != myStr[symmetry])
+            if (latter == former)
+            { // 如果latter和former指向的是同一个字符，说明这个字符【落单】了
+                if (isOdd && !hasAlone)
                 {
-                    latter = i; // 找到了相同的字符
-                    break;
+                    // 不用刻意移动这个字符，只需要把落单字符【移动到正中间的交换次数】加上即可
+                    // 结果只需要一个数字罢了
+                    swapCount += (middleInt - former);
+                }
+                else
+                {
+                    printf("Impossible");
+                    return 0;
                 }
             }
-        }
-        // latter如果还是-1，说明没找到与former相同的字符, former指向的这个字符【落单了】
-        if (latter == -1)
-        {
-            // 对于【落单字符】的处理
-            // 在【回文串长度为奇数】时，有且仅能有【一个落单字符】
-            if (isOdd && !hasAloneChar)
+            else if (myStr[latter] == myStr[former]) // 如果找到了相同的字符
             {
-                destInd = middleInd; // 唯一的落单字符【只能在回文串的最中间】, 所以将终点下标设为middleInd
-                hasAloneChar = true; // 标记已经有落单字符了
-            }
-            else
-            {
-                // 其他情况，那就不可能形成回文串了，程序结束
-                cout << "Impossible";
-                return 0;
+                // latter和former不是同一个字符，就把latter指向的字符移动到和former对称的symmetry处
+                for (; latter < symmetry; latter++)
+                {
+                    char temp = myStr[latter + 1];
+                    myStr[latter + 1] = myStr[latter];
+                    myStr[latter] = temp;
+                    swapCount++;
+                }
+                symmetry--;
+                break;
             }
         }
-        // 接下来将
     }
+    printf("%d", swapCount);
     return 0;
 }
