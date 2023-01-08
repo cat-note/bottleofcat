059df5038554293605a8c291212fb3efb01b53b0
SomeBottle <somebottle@outlook.com>
2023-01-08 18:59:05 +0800

    Selection Sort

--- Algo/code/C-Cpp/PTA/DataStructure/DS-7-12.cpp
+++ Algo/code/C-Cpp/PTA/DataStructure/DS-7-12.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 typedef vector<long> Sequence; // 长整型数字序列
 
-void BubbleSort(Sequence &seq); // 冒泡排序
+void BubbleSort(Sequence &seq);    // 冒泡排序
+void SelectionSort(Sequence &seq); // 选择排序
 
 int main()
 {
@@ -16,7 +17,8 @@ int main()
     for (int i = 0; i < N; i++)
         scanf(" %ld", &seq[i]);
     // 进行排序
-    BubbleSort(seq);
+    // BubbleSort(seq);
+    SelectionSort(seq);
     // 输出序列
     for (int i = 0; i < N; i++)
     {
@@ -35,9 +37,11 @@ void Swap(long &elem1, long &elem2)
     elem2 = tmp;
 }
 
-// 冒泡排序
-// 冒泡排序其实就是一个不断交换的过程
-// 经过测试，冒泡排序在对【数据5】和【数据9】进行排序时会TLE(超时)
+// [冒泡排序]
+// 冒泡排序其实就是一个【数组内】不断交换的过程，因为【仅仅是交换数字】，这个算法的表现【很稳定】
+// 最好情况: 第一次扫描时，整个数组已经有序了，O(n)
+// 最糟情况: 两个循环都跑完才排序完毕，O(n^2)
+//     * 经过测试，冒泡排序在对【数据5】和【数据9】进行排序时会TLE(超时)
 void BubbleSort(Sequence &seq)
 {
     // 冒泡排序原地完成，不需要额外空间
@@ -59,5 +63,29 @@ void BubbleSort(Sequence &seq)
         // 如果没发生交换说明已经排序完成
         if (!exchanged)
             break;
+    }
+}
+
+// [选择排序]
+// 选择排序也是数组内交换，不过有一个【选择数字进行交换】的过程，因此也是原地算法。
+//     * 因为有这个选择的过程，选择排序是不稳定的，相同值的数字在不同排序过程中可能顺序不同
+// 选择排序无论如何都需要把两个循环都跑完, 因此无论最好情况还是最坏情况【时间复杂度全为O(n^2)】，是相对耗时的排序算法
+//     * 经过测试，选择排序只能通过【前四个测试点】，数据量大的情况下非常不适合
+void SelectionSort(Sequence &seq)
+{
+    int len = seq.size();
+    // 选择排序也是原地完成的
+    for (int i = 0; i < len; i++)
+    {
+        int selection = i;
+        // 在[i+1,len-1]下标范围的子序列中“选择”最小的，且比seq[i]小的数
+        for (int j = i + 1; j < len; j++)
+        {
+            if (seq[j] < seq[selection])
+                selection = j;
+        }
+        // 如果找到了比seq[i]更小的数就交换
+        if (selection != i)
+            Swap(seq[selection], seq[i]);
     }
 }
