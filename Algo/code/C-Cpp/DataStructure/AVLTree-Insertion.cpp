28aabd1a61c03a0d98e139548e9f0ec930ccc50e
SomeBottle <somebottle@outlook.com>
2023-01-12 00:24:28 +0800

    Update note and code

--- Algo/code/C-Cpp/DataStructure/AVLTree-Insertion.cpp
+++ Algo/code/C-Cpp/DataStructure/AVLTree-Insertion.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ struct TreeNode
 {
     Tree left;  // 左子树
     Tree right; // 右子树
-    short bf;   // 平衡因子
+    int height; // 节点所在高度，平衡因子靠这个算
     int val;    // 节点值
 };
 
@@ -17,9 +18,127 @@ class AVLTree
 private:
     Tree tree = NULL; // 空树
 public:
+    // 更新节点高度
+    void updateHeight(Tree node)
+    {
+        int leftHeight = node->left ? node->left->height : 0;
+        int rightHeight = node->right ? node->right->height : 0;
+        // 选择较高的那颗子树的高度+1以更新
+        if (leftHeight > rightHeight)
+            node->height = leftHeight + 1;
+        else
+            node->height = rightHeight + 1;
+    }
+    // 获得某个节点的平衡因子
+    int balanceFactor(Tree node)
+    {
+        // 左子树高度-右子树高度
+        int leftHeight = node->left ? node->left->height : 0;
+        int rightHeight = node->right ? node->right->height : 0;
+        return leftHeight - rightHeight;
+    }
+    // 失衡节点右旋操作，node是失衡结点
+    void rotateRight(Tree node)
+    {
+        Tree nodeLeft = node->left;         // 失衡节点左子树
+        Tree nodeRight = node->right;       // 失衡节点右子树
+        Tree lChildLeft = nodeLeft->left;   // 失衡节点的左孩子的左子树
+        Tree lChildRight = nodeLeft->right; // 失衡节点左孩子的右子树
+        // 这里【没有指向父节点】的指针，我们直接修改结点的值来模拟移动结点即可
+        int nodeVal = node->val;   // 失衡节点的值
+        node->val = nodeLeft->val; // 交换失衡节点和左孩子的值
+        nodeLeft->val = nodeVal;
+        // 这里已经不是左孩子了，而是“旋转”下来的失衡节点
+        nodeLeft->left = lChildRight; // 修改结点的左右子树
+        nodeLeft->right = node->right;
+        node->left = lChildLeft; // 和子树的根结点接上
+        node->right = nodeLeft;
+        // 此时node是子树根结点，lChildLeft是左子树，nodeLeft是右子树
+        updateHeight(nodeLeft); // 更新有变动的结点的高度，先更新子树再更新根
+        updateHeight(node);
+    }
     // 插入操作
-    void insert(int value){
-
+    void insert(int value)
+    {
+        Tree node = new TreeNode();
+        node->left = NULL;
+        node->right = NULL;
+        node->val = value;
+        if (!tree) // 树还是空的，作为首个节点
+        {
+            tree->height = 1; // 叶节点高度为1
+            tree = node;
+            return;
+        }
+        // 接下来是树不为空的情况，寻找插入的位置
+        Tree curr = tree;
+        stack<Tree> access; // 储存插入节点经过的路径的栈，因为后面要回溯
+        while (true)
+        {
+            access.push(curr);
+            if (value < curr->val)
+            {
+                // 插入值比这个节点要小，往左子树插
+                if (curr->left)
+                {
+                    curr = curr->left; // 左子树不为空，进入左子树
+                }
+                else
+                {
+                    curr->left = node; // 左子树为空，作为左子树根结点
+                    break;
+                }
+            }
+            else if (value > curr->val)
+            {
+                // 插入值比这个节点要大，往右子树插
+                if (curr->right)
+                {
+                    curr = curr->right; // 右子树不为空，进入右子树
+                }
+                else
+                {
+                    curr->right = node; // 右子树为空，作为右子树根结点
+                    break;
+                }
+            }
+            else
+            {
+                // 找到了相同值的节点，不予插入
+                delete node;
+                return;
+            }
+        }
+        // 插入完成后回溯，检查沿路节点的平衡因子
+        while (!access.empty())
+        {
+            // 弹栈
+            Tree curr = access.top();
+            access.pop();
+            // 更新节点所在高度
+            updateHeight(curr);
+            // 获得节点平衡因子
+            int bf = balanceFactor(curr);
+            // 如果插入后，该节点的平衡因子=0，就不用再回溯了，这说明插入的节点并不会影响平衡
+            if (bf == 0)
+                break;
+            // 该节点失衡了，需要进行调整
+            if (bf > 1)
+            {
+                // 失衡的平衡因子>1，说明左子树比较高，因此找失衡节点的左孩子
+                // 看失衡节点左孩子的平衡因子
+                int leftBf = balanceFactor(curr->left);
+                if (leftBf > 0)
+                {
+                    // 这个左孩子的左子树高于右子树
+                    // 这说明是LL型，即插入在失衡节点【左孩子的左子树中】而导致失衡，需要进行“右旋”进行调整
+                    rotateRight(curr);
+                }else{
+                    // 这个左孩子的右子树高于左子树
+                    // 这说明是LR型，插入在失衡结点【左孩子的右子树中】而导致失衡，需要进行
+                }
+            }
+        }
     }
 };
 
