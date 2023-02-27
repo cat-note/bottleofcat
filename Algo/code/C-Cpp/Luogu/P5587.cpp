c423252593b18fd086162e622cd8944d4b6f5e24
SomeBottle <somebottle@outlook.com>
2023-02-27 12:52:18 +0800

    Finally AC!

--- Algo/code/C-Cpp/Luogu/P5587.cpp
+++ Algo/code/C-Cpp/Luogu/P5587.cpp
@@ -27,7 +27,7 @@ int readText(string *arr)
     int line = 0;     // 当前读取的是第line行
     while (true)
     {
-        scanf("%c", &tmp[tmpLen]);                      // 逐字符读入
+        tmp[tmpLen] = getchar();                        // 逐字符读入
         if (tmp[tmpLen] == '\r' || tmp[tmpLen] == '\n') // 如果遇到了换行符，说明一行读取结束
         {
             tmp[tmpLen] = '\0'; // 忽略换行符
@@ -39,7 +39,15 @@ int readText(string *arr)
         }
         else
         {
-            tmpLen++;
+            if (tmp[tmpLen] == '<') // 如果读入的是一个退格键
+            {
+                if (tmpLen > 0) // 防止越下界
+                    tmpLen--;   // 模拟回退
+            }
+            else
+            {
+                tmpLen++;
+            }
         }
     }
     return line;
@@ -52,54 +60,60 @@ int inputText(string *model, int lineNum)
     int cmpInd = 0;      // 比较指针
     int line = 0;        // 当前读取的是第line行
     int correct = 0;     // 记录输入正确的字符数
-    bool ignore = false; // 是否开始忽略
+    bool ignore = false; // 是否开始忽略多余的行
     while (true)
     {
-        scanf("%c", &tmp[tmpLen]); // 逐字符读入
-        if (line >= lineNum)       // 输入的行数多于了范文，忽略多余内容
-            ignore = true;
+        tmp[tmpLen] = getchar();                        // 逐字符读入
         if (tmp[tmpLen] == '\r' || tmp[tmpLen] == '\n') // 如果遇到了换行符，说明一行读取结束
         {
-            tmp[tmpLen] = '\0'; // 忽略换行符
-            // 遇到EOF，就说明读完了当前文本段
-            if (tmp[0] == 'E' && tmp[1] == 'O' && tmp[2] == 'F')
+            tmp[tmpLen] = '\0';                                  // 忽略换行符
+            if (tmp[0] == 'E' && tmp[1] == 'O' && tmp[2] == 'F') // 遇到EOF，就说明读完了当前文本段
+            {
                 break;
+            }
+            else if (!ignore) // 否则，如果读入的内容没有超出范文的行数，就逐字符比较读入的内容和范文
+            {
+                int minLen = tmpLen > model[line].length() ? model[line].length() : tmpLen; // 找出较短的一条字符串的长度
+                for (int i = 0; i < minLen; i++)
+                {
+                    if (tmp[i] == model[line][i])
+                        correct++;
+                }
+            }
             tmpLen = 0; // 临时字符数归零
             cmpInd = 0; // 比较指针归零
             line++;
-        }
-        else if (ignore) // 忽略多余的输入
-        {
-            tmpLen++;
+            if (line >= lineNum) // 输入的行数多于了范文，忽略多余内容
+                ignore = true;
         }
         else
         {
             if (tmp[tmpLen] == '<')
             {
-                if (cmpInd > 0) // 比较指针最多回退到0
-                    cmpInd--;   // 比较指针回退，模拟退格
-            }
-            else if (model[line][cmpInd] == tmp[tmpLen]) // 如果指定位置上的字符一致，就统计在内
-            {
-                correct++;
-                cmpInd++;
+                if (tmpLen > 0) // 模拟回退
+                    tmpLen--;   // 读取指针回退，模拟退格
             }
             else
             {
-                cmpInd++;
+                tmpLen++;
             }
-            tmpLen++;
-            // 注意，length方法返回的是无符号整数，需要转换一下
-            // 不然编译器会把cmpInd转换为无符号。
-            if (cmpInd >= (int)model[line].length()) // 防止用户输入过长导致越界
-                cmpInd = (int)model[line].length();
         }
     }
     return correct; // 返回输入正确的数量
 }
 
 /*
-    因为退格键仅限当前行，且回车符不会计入正确字符数，因此本题按行处理比较合适，而且很适合【边读入边处理】。
+    因为退格键仅限当前行，且回车符不会计入正确字符数，因此本题按行处理比较合适。
 
-    逆天，看了题解才知道这题的范文也有退格键<，这什么鬼题面...
+    * 吐槽:  
+        逆天，看了题解才知道这题的范文也有退格键<，这什么鬼题面...
+        我最开始是按【边读入边和范文逐字符对比】的方式去写的，但我怎么改都没法AC...
+        于是我索性【读完后】再和范文逐字符对比，这样一写就能AC了...想不到就是这样简单，我还以为会有TLE惊喜来着.
+
+    现在看来，这题其实是不错的模拟题。
+    ---------------------------------
+    这题我的思路就是：
+        1. 先逐行且逐字符读入字符串，然后遇到'<'时，读取指针【就往回退一位】，反之则前进一位，以模拟退格过程。
+            *
+    
 */
